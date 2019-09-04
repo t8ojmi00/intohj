c26b823a78fe3466af658f788acda491add55b7b
Ojanlatva Mika <OJANLMI@steellan.com>
2019-09-04 19:53:24 +0300

    html menulle valmis

---
+++ menu.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Internet ohjelmoinnin harjoitustyö</title>
+    <link rel="stylesheet" href="css/mystyle.css">
+  </head>
+  <body>
+  <div id="header">
+    This is the header
+  </div>
+  <div id="menu">
+    Menu
+  </div>
+  <div id="content">
+    This is the content
+  </div>
+  <div id="footer">
+    This is the footer
+  </div>
+  </body>
+</html>
