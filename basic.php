5d695affea12d7e742890fc06e7aa2dd0a41724d
Mika <t8ojmi00@students.oamk.fi>
2019-09-10 12:22:24 +0300

    menu ja sivupohjat tehty

---
+++ basic.php
@@ -0,0 +1,6 @@
+<?php include "menu.php"; ?>
+	<h2>PHP basic</h2>
+	<p>
+		This is the PHP basic.
+	</p>
+<?php include "footer.php"; ?>
--- menu.php
+++ menu.php
@@ -10,13 +10,16 @@
     This is the header
   </div>
   <div id="menu">
-    Menu
+    <ul>
+		  <li><a href="index.php">Main page</a></li>
+		  <li><a href="basic.php">PHP Basic</a></li>
+		  <li><a href="arrays.php">PHP Arrays</a></li>
+		  <li><a href="post.php">PHP POST</a></li>
+		  <li><a href="get.php">PHP GET</a></li>
+		  <li><a href="database.php">Database</a></li>
+		  <li><a href="session.php">Session</a></li>
+	 </ul>
   </div>
   <div id="content">
     This is the content
-  </div>
-  <div id="footer">
-    This is the footer
-  </div>
-  </body>
-</html>
+  
