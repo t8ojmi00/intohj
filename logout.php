f58bc8a4e2b8936b293cdc457d190df5208765b1
Mika Ojanlatva <t8ojmi00@students.oamk.fi>
2019-09-30 18:19:41 +0300

    menu ja logout päivitetty

---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php include "menu.php"; ?>
+<?php
+  session_start();
+  session_destroy();
+ ?>
+ <p>
+   Olet kirjautunut ulos.
+ </p>
+<?php include "footer.php"; ?> 
--- menu.php
+++ menu.php
@@ -17,9 +17,10 @@
 		  <li><a href="post.php">PHP POST</a></li>
 		  <li><a href="get.php">PHP GET</a></li>
 		  <li><a href="database.php">Database</a></li>
+      <li><a href="login.php">Login</a></li>
+      <li><a href="logout.php">logout</a></li>
 		  <li><a href="session.php">Session</a></li>
 	 </ul>
   </div>
   <div id="content">
     This is the content
-  
