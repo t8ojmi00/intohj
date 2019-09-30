96545d7c163d69771f22830bc4418cdecaf624d6
Mika Ojanlatva <t8ojmi00@students.oamk.fi>
2019-09-30 18:29:58 +0300

    database päivitetty

--- database.php
+++ database.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+if(!isset($_session['logged_in'])) {
+	header('Location:not_logged.php');
+}
+ ?>
 <?php include "menu.php"; ?>
 	<h2>Database</h2>
 	<p>
---
+++ not_logged.php
@@ -0,0 +1,5 @@
+<?php include "menu.php"; ?>
+<p>
+  Sinun pitää kirjautua sisään.
+</p>
+<?php include "footer.php" ?>
