b7bb1f2de3feac22d2f81773e67e64b2395d36eb
Mika Ojanlatva <t8ojmi00@students.oamk.fi>
2019-09-30 19:01:06 +0300

    menu päivitetty

--- menu.php
+++ menu.php
@@ -16,7 +16,7 @@
 		  <li><a href="arrays.php">PHP Arrays</a></li>
 		  <li><a href="post.php">PHP POST</a></li>
 		  <li><a href="get.php">PHP GET</a></li>
-      <li><a href="login.php">Login</a></li>
+      <li><a href="login_form.php">Login</a></li>
       <li><a href="logout.php">Logout</a></li>
 		  <li><a href="database.php">Database</a></li>
 		  <li><a href="session.php">Session</a></li>
